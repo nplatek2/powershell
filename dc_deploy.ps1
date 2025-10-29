d50d6cbf7e930700c705102f36941778ddf79764
Nathaniel Platek <nathan.platek@outlook.com>
2025-10-29 14:16:32 -0400

    Copy of dc-deploy script from repo

---
+++ dc_deploy.ps1
@@ -0,0 +1,18 @@
+{
+  "VMName": "DC",
+  "VMPath": "V:\\VM",
+  "Generation": 2,
+  "MemoryStartupBytes": 4294967296,
+  "DynamicMemoryEnabled": true,
+  "SwitchName": "vInternal",
+  "ProcessorCount": 4,
+  "AutomaticCheckpointsEnabled": false,
+  "DifferencingDisk": {
+    "Name": "dc_hd.vhdx",
+    "Path": "V:\\Disk_Image\\dc_hd.vhdx",
+    "ParentPath": "V:\\Parent_Image\\server2025_hd.vhdx",
+    "ControllerType": "SCSI",
+    "ControllerNumber": 0,
+    "ControllerLocation": 0
+  }
+}
